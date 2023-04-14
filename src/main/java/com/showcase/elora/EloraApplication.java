719db5dbb2f98473b60092e0b69e6c1a72df84d3
Kevin Krisna <kevinkrisnadp98@gmail.com>
2023-04-14 15:22:33 +0700

    init

---
+++ src/main/java/com/showcase/elora/EloraApplication.java
@@ -0,0 +1,13 @@
+package com.showcase.elora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showcase/elora/EloraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showcase.elora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EloraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
