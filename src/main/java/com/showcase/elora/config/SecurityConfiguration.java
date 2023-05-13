efe62ababc82e717ca0627787a6b4e50b64de60e
Nicholas Krisna <nicholaskrisnapl@gmail.com>
2023-05-13 17:22:58 +0700

    adding few pattern url that will be authenticated

--- src/main/java/com/showcase/elora/config/SecurityConfiguration.java
+++ src/main/java/com/showcase/elora/config/SecurityConfiguration.java
@@ -4,12 +4,18 @@ import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +31,11 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
-                .permitAll()
+//                .requestMatchers("")
+//                .permitAll()
+                .requestMatchers("/api/v1/store/**").hasAnyRole("store")
+                .requestMatchers("/api/v1/admin/**").hasAnyRole("admin")
+                .requestMatchers("/api/v1/customer/**").hasAnyRole("customer")
                 .anyRequest()
                 .authenticated()
                 .and()
